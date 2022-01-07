7eefc98dc9280ff413c35e4bb72d46635e51a384
unknown <robert@epogue.com>
2022-01-07 14:27:25 -0500

    Add -o option to output directly to file.

--- src/main.rs
+++ src/main.rs
@@ -18,18 +18,23 @@ fn main() {
             panic!("not enough arguments. provide length of one-time-pad key");
         }
         let key_len_mb = &args[2].parse::<usize>().expect("Failed to parse int argument to `-g` flag");
+
+        let mut out: Box<dyn Write> = Box::new(io::stdout());
+        if args.len() >= 5 && args[3] == "-o" {
+            out = Box::new(File::create(&args[4]).expect("Unable to create file"));
+        }
+
         const BYTES_PER_MEGABYTE:usize = 1024 * 1024; // 1024 bytes -> kb, 1024 kb -> mb
         let key_len_bytes = key_len_mb * BYTES_PER_MEGABYTE;
         let mut rng = rand::thread_rng();
 
         const MAX_CHUNK_SIZE_BYTES: usize = BYTES_PER_MEGABYTE/2;
         let mut chunk = [0u8; MAX_CHUNK_SIZE_BYTES];
-        let mut out = io::stdout();
+
         let mut bytes_written = 0;
         while bytes_written < key_len_bytes {
             let bytes_to_write = (key_len_bytes - bytes_written).clamp(0,MAX_CHUNK_SIZE_BYTES);
             rng.fill(&mut chunk[..bytes_to_write]);
- 
             bytes_written += out.write(&chunk).expect("Output Error. Try piping to a file.");
         }
     } else {
